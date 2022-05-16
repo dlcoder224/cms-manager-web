11749a9fb662ffcb56dd9b2d2c25cfba0e758e42
liuwenlong <liuwenlong@111.com>
2022-05-16 21:14:51 +0800

    axios 二次封装

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import request from '@/config'
+import storage from '@/utils/storge'
 
 console.log(import.meta.env)
 const app = createApp(App)
 
 app.config.globalProperties.$http = request
+app.config.globalProperties.$storage = storage
 app.use(router).mount('#app')
